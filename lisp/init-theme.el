c9430c021f3e132267383da945ba6a7f8d6e9a70
Ashish Vidyarthi <ashish.vid@gmail.com>
2013-12-11 01:35:29 -0800

    Fix theme settings and reorganize emac themes

--- lisp/init-theme.el
+++ lisp/init-theme.el
@@ -1,14 +1,28 @@
 ;; Set custom theme path
+;; Set path to additional files
+
+
 (setq custom-theme-directory (concat user-emacs-directory "themes"))
 
-(dolist
-    (path (directory-files custom-theme-directory t "\\w+"))
-  (when (file-directory-p path)
-    (add-to-list 'custom-theme-load-path path)))
+(add-to-list 'custom-theme-load-path (concat user-emacs-directory "themes/soft-stone-theme"))
+(add-to-list 'custom-theme-load-path (concat user-emacs-directory "themes/zenburn"))
+(add-to-list 'custom-theme-load-path (concat user-emacs-directory "themes/noctilux-theme"))
+(add-to-list 'custom-theme-load-path (concat user-emacs-directory "themes/emacs-leuven-theme"))
+(add-to-list 'custom-theme-load-path (concat user-emacs-directory "themes/soft-charcoal-theme"))
+(add-to-list 'custom-theme-load-path (concat user-emacs-directory "themes/ample-theme"))
+(add-to-list 'custom-theme-load-path (concat user-emacs-directory "themes/solarized-emacs"))
 
-;; setup custom theme
- (load-theme 'zenburn t)
- (set-cursor-color "red")
+;; Highlight current line
+;; (global-hl-line-mode 1)
+;; (set-cursor-color "#ffffff")
 
+;;;; setup custom theme
+;; (load-theme 'zenburn t)
+;; (load-theme 'solarized-light t)
+;; (load-theme 'noctilux t)
+;; (load-theme 'leuven t)
+;; (load-theme 'soft-charcoal t)
+;;(load-theme 'soft-stone t)
+(load-theme 'ample t)
 
 (provide 'init-theme)
--- themes/zenburn-theme.el
+++
@@ -1,1 +0,0 @@
-zenburn/zenburn-theme.el
